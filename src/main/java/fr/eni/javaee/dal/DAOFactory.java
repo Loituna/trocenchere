ae47af36e7278ea8dbf8060538621959b634f900
ttaillandier2023 <ttaillandier2023@129SE2-HD4MMS3.ad.campus-eni.fr>
2023-03-28 11:39:46 +0200

    DAOFactory

--- src/main/java/fr/eni/javaee/dal/DAOFactory.java
+++ src/main/java/fr/eni/javaee/dal/DAOFactory.java
@@ -1,20 +1,13 @@
 package fr.eni.javaee.dal;
 
-<<<<<<< HEAD
 public abstract class DAOFactory {
 
-	public static UtilisateurDAO getUtilisateurDAO () {
+	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOJdbcImpl();
 	}
-	
-	public static EnchereDAO getEnchereDAO () {
-		return new EnchereDAOJdbcImpl();
-=======
-public class DAOFactory {
 
-	
-	public static UtilisateurDAO getInstance() {
-		return new UtilisateurImpl();
->>>>>>> branch 'master' of https://github.com/Loituna/trocenchere.git
+	public static EnchereDAO getEnchereDAO() {
+		return new EnchereDAOJdbcImpl();
+
+		}
 	}
-}
