8ed219b6d1002406633b1dd2d4a83d3788cd0bd7
Irwin Le Bihan <irwnin.lebihan2023@campus-eni.fr>
2023-03-28 11:06:46 +0200

    DaoFacto + servle coo

--- src/main/java/fr/eni/javaee/dal/UtilisateurImpl.java
+++ src/main/java/fr/eni/javaee/dal/UtilisateurImpl.java
@@ -1,5 +1,43 @@
 package fr.eni.javaee.dal;
 
-public class UtilisateurImpl {
+import fr.eni.javaee.bo.Utilisateur;
+
+public class UtilisateurImpl implements UtilisateurDAO{
+
+	@Override
+	public void creationUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificationUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suppressionUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void authentificationUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deconnexionUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
