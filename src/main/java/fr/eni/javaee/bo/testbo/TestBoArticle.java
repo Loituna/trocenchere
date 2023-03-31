8e3146bd14dbd9a8a62d9c529147400891fcc7e0
sdufeil2023 <sdufeil2023@129SE2-7D4MMS3.ad.campus-eni.fr>
2023-03-31 14:02:11 +0200

    push testbo article wip

---
+++ src/main/java/fr/eni/javaee/bo/testbo/TestBoArticle.java
@@ -0,0 +1,35 @@
+package fr.eni.javaee.bo.testbo;
+
+import java.time.LocalDateTime;
+
+import fr.eni.javaee.bo.Article;
+
+public class TestBoArticle {
+
+	public static void main(String[] args) {
+		
+	// Création des dateDebutEnchere et dateFinEnchere
+	
+	// dateDebutEnchere initialisée à la date actuelle
+	LocalDateTime dateDebutEnchere = LocalDateTime.now();
+	// dateFinEnchere initialisée à la date actuelle + 9 heures
+	LocalDateTime dateFinEnchere = LocalDateTime.now().plusHours(9);
+	
+	// Création d'un nouvel article
+	Article article = new Article();
+	
+	article.setNoArticle(1);
+	article.setNomArticle("carte graphique");
+	article.setDescription("carte de ouf");
+	article.setDateDebutEnchere(dateDebutEnchere);
+	article.setDateFinEnchere(dateFinEnchere);
+	article.setPrixInitial(100);
+	article.setPrixVente(222);
+	article.setEtatVente(true);
+	article.setNoCategorie(null);
+	article.setNoUtilisateur(null);
+	
+	System.out.println(article.toString());
+	
+	}
+}
