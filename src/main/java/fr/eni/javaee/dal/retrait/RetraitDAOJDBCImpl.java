58a075365f47cde002dc9eb7727809e724090d2f
ttaillandier2023 <ttaillandier2023@129SE2-HD4MMS3.ad.campus-eni.fr>
2023-03-31 13:57:24 +0200

    Methode insert retrait

--- src/main/java/fr/eni/javaee/dal/retrait/RetraitDAOJDBCImpl.java
+++ src/main/java/fr/eni/javaee/dal/retrait/RetraitDAOJDBCImpl.java
@@ -1,9 +1,55 @@
 package fr.eni.javaee.dal.retrait;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import fr.eni.javaee.bll.BusinessException;
+import fr.eni.javaee.bo.Retrait;
+import fr.eni.javaee.dal.tools.CodesResultatDAL;
+import fr.eni.javaee.dal.tools.ConnectionProvider;
+
 class RetraitDAOJDBCImpl implements RetraitDao {
 
+	private static final String INSERT = "INSERT INTO RETRAIT(rue, code_postal, ville) VALUES (?,?,?)";
 
+	@Override
+	public void insert(Retrait retrait) throws BusinessException {
+		if (retrait == null) {
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.INSERT_RETRAIT_NULL);
+			throw businessException;
+		}
+		PreparedStatement pstmt = null;
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+			pstmt = cnx.prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, retrait.getRue());
+			pstmt.setString(1, retrait.getCodePostal());
+			pstmt.setString(1, retrait.getVille());
+			ResultSet rs = pstmt.getGeneratedKeys();
+			if (rs.next()) {
+				retrait.setNoArticle(rs.getInt(1));
+				retrait.setRue(rs.getString(2));
+				retrait.setCodePostal(rs.getString(3));
+				retrait.setVille(rs.getString(4));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.INSERT_RETRAIT_ECHEC);
+			throw businessException;
+		} finally {
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();/* , PreparedStatement.RETURN_GENERATED_KEYS */
+				}
+		}
 
-	
-	//TODO DAL MYSQL Retrait
+	}
+
+	// TODO DAL MYSQL Retrait
 }
---
+++ src/main/java/fr/eni/javaee/servlet/test/ServletTestRetrait.java
@@ -0,0 +1,58 @@
+package fr.eni.javaee.servlet.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.javaee.bll.BusinessException;
+import fr.eni.javaee.bo.Retrait;
+import fr.eni.javaee.dal.DAOFactory;
+
+/**
+ * Servlet implementation class ServletTestRetrait
+ */
+@WebServlet("/ServletTestRetrait")
+public class ServletTestRetrait extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTestRetrait() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		Retrait RRRrrr = new Retrait("5 rue de la retraite", "35000", "Rennes");
+		
+		try {
+			DAOFactory.getRetraitDao().insert(RRRrrr);
+			
+		}catch(BusinessException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(RRRrrr);
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
