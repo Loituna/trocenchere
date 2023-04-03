544ee61aabcdc61f1ebb39979f9cddf03486d53a
ttaillandier2023 <ttaillandier2023@129SE2-HD4MMS3.ad.campus-eni.fr>
2023-04-03 13:12:00 +0200

    encherir

--- src/main/java/fr/eni/javaee/servlet/Enchere/ServletNouvelleEnchere.java
+++ src/main/java/fr/eni/javaee/servlet/Enchere/ServletNouvelleEnchere.java
@@ -13,13 +13,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class ServletEnchere
  */
 @WebServlet("/ServletEnchere")
-public class ServletEnchere extends HttpServlet {
+public class ServletNouvelleEnchere extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletEnchere() {
+    public ServletNouvelleEnchere() {
         super();
         // TODO Auto-generated constructor stub
     }
